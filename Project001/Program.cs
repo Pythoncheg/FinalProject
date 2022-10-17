6242c48e56f28df4ee37a0df88b505061ae9317a
Denis_Gerasimov <Pythonchic@gmail.com>
2022-10-17 22:09:16 +0300

    Описал метод распечатывания массива

--- Project001/Program.cs
+++ Project001/Program.cs
@@ -9,3 +9,13 @@ string[] CreateArray(int size)
 {
     return new string[size];
 }
+
+void PrintArray(string[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        Console.Write($"{arr[i]}");
+
+    }
