6987e3c2fec373d4ecb3d83c97d9712a47f9245f
Denis_Gerasimov <Pythonchic@gmail.com>
2022-10-17 22:07:18 +0300

    Добавил метод нахождения псевдослучайного числа

--- Project001/Program.cs
+++ Project001/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int GetValue(int min, int max)
+{
+    return new Random().Next(min, max);
+}
